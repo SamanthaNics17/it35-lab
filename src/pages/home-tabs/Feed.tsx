98ce30881e4fad74c2cf46d347aab1d083207b07
SamanthaNics17 <20211740@nbsc.edu.ph>
2025-04-06 10:22:37 +0800

    Added Search and Feed Contents.

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,36 +1,58 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >Feed
-        </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonButton,
+} from "@ionic/react";
+const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Samantha</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          </IonCardHeader>
 
-  export default Feed;
+          <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+
+          <IonButton fill="clear">Action 1</IonButton>
+          <IonButton fill="clear">Action 2</IonButton>
+        </IonCard>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Nicole</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+
+          <IonButton fill="clear">Action 1</IonButton>
+          <IonButton fill="clear">Action 2</IonButton>
+        </IonCard>
+        
+      </IonContent>
+
+
+    </IonPage>
+  );
+};
+
+export default Feed;
