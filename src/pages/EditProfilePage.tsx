d505588dc2554db01ad7b4eb3195088176b1ed5f
SamanthaNics17 <20211740@nbsc.edu.ph>
2025-04-23 08:49:49 +0800

    Modified the EditProfile.tsx

--- src/pages/EditProfilePage.tsx
+++ src/pages/EditProfilePage.tsx
@@ -4,10 +4,10 @@ import {
   IonBackButton, IonButtons, IonItem, IonText, IonCol, IonGrid,
   IonRow, IonInputPasswordToggle, IonImg, IonAvatar,
 } from '@ionic/react';
-import supabase from '../utils/supabaseClient';
+import  supabase  from '../utils/supabaseClient';
 import { useHistory } from 'react-router-dom';
 
-const EditAccount: React.FC = () => {
+const EditProfile: React.FC = () => {
     const [email, setEmail] = useState('');
     const [currentPassword, setCurrentPassword] = useState('');
     const [password, setPassword] = useState('');
@@ -323,4 +323,4 @@ const EditAccount: React.FC = () => {
     );
   };
   
-  export default EditAccount;
+  export default EditProfile;
